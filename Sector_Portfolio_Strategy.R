d6ada5e90fe04ce15c9682621a3d86ee62e6d37e
Linus <Linus_Kaiser@outlook.de>
2021-11-08 01:36:59 +0100

    #00004 Implementation of the Sector_Indicator_Strategy
    created df with abnormal returns of SP firms and base function for deriving the 5 days correlations of abnormal and SP returns

--- Sector_Portfolio_Strategy.R
+++ Sector_Portfolio_Strategy.R
@@ -23,6 +23,56 @@ str(SP500_stock_returns$df.tickers)
 View(SP500_stock_returns$df.tickers)
 tq_performance_fun_options()
 
-Ra <- SP500_stock_returns$df.tickers %>%
-  group_by(ticker) %>%
-  tq_performance(Ra = ret.closing.prices, Rb = Null, performance_fun = CAPM.beta)
+
+just_SP500_returns <- select(SP500_returns$df.tickers, ref.date, ret.closing.prices)
+grouped_SP500_stock_returns <- SP500_stock_returns$df.tickers %>%
+  group_by(ticker)
+  
+
+RaRb <- left_join(grouped_SP500_stock_returns, just_SP500_returns, by = c("ref.date" = "ref.date"))
+SP500_Betas <- tq_performance(RaRb,Ra = ret.closing.prices.x, Rb = ret.closing.prices.y, performance_fun = CAPM.beta)
+
+
+#Mechanism to calculate stock specific returns
+#daywise_buy/sell_indicator
+#
+#Indicator
+#Abnormal_daily_return-correlation_delta_(*5*)_days
+#
+
+indicator_df <- select(RaRb,ticker,ref.date,ret.closing.prices.x,ret.closing.prices.y) 
+colnames(indicator_df) <- c("ticker", "ref.date", "ret.closing.prices.stock", "ret.closing.prices.SP")
+
+
+abnormal_daily_returns_df <- indicator_df %>% 
+  select(ticker, ref.date, ret.closing.prices.stock, ret.closing.prices.SP) %>% 
+  ungroup() %>% 
+  mutate(ret.abnormal = indicator_df$ret.closing.prices.stock-indicator_df$ret.closing.prices.SP) %>%
+  group_by(ticker)
+
+correlation_delta_5_days
+correlation_vector <- vector(length = 0)
+for (row in 1:length(abnormal_daily_returns_df$ret.closing.prices.SP)){
+  if ((row-5) < 1){
+    counter <- 1
+  }
+  print(row)
+  if (row>1){
+    correlation_5_days <- cor(abnormal_daily_returns_df$ret.closing.prices.SP[counter:row],abnormal_daily_returns_df$ret.abnormal[counter:row],use="complete.obs")
+    correlation_vector <- append(correlation_vector,correlation_5_days)
+  }
+}
+cor_returns_df <- abnormal_daily_returns_df %>% mutate(correlation = correlation_5_days)
+print(cor_returns_df)
+  
+abnormal_daily_returns_df$ret.closing.prices.SP[1:3]
+
+indicator_df <- mutate(ungroup(indicator_df), ret.abnormal = indicator_df$ret.closing.prices.stock-indicator_df$ret.closing.prices.SP, Indicator = abnormal_daily_returns_df$ret.abnormal-correlation_delta_5_days)
+
+
+for (row in 1:length(abnormal_daily_returns_df$ret.closing.prices.SP)){
+  print(row)
+}
+
+
+cor(abnormal_daily_returns_df$ret.closing.prices.SP[1:1],abnormal_daily_returns_df$ret.abnormal[1:1],use="complete.obs")
